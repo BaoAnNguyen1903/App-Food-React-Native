0c2215e201bb00ac80050dc27fe620be459e9af5
An Nguyen <anndb1903@gmail.com>
2025-05-18 23:49:44 +0700

    Home

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Detai} />
+        <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
