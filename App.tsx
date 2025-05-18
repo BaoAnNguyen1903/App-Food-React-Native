f66cd3dacb82e9a5be8720df08fdcc7f59013665
An Nguyen <anndb1903@gmail.com>
2025-05-18 23:50:05 +0700

    Like

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Home" component={Like} />
       </Stack.Navigator>
     </NavigationContainer>
   );
